358c7d5ae433cd39e564961e797a58b631ca9912
SARBAN MENDALI <sarbanmendali.007@gmail.com>
2026-01-08 15:36:12 +0000

    js modified

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ data.forEach((x,n)=>{
     const c=document.createElement("div");
     c.className="card";
     c.innerHTML=`<div class="question">${n+1}. ${x.q}</div>
-                 <button class="action-btn">Show Answer</button>
+                 <button class="action-btn">Show / Hide Answer</button>
                  <div class="answer">${x.a}</div>`;
     c.querySelector("button").onclick=()=> {
         const ans=c.querySelector(".answer");
